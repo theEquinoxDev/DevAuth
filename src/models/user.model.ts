c745753f3f8ac958bfebf50499eed4bd96d4f656
Aditya <adityasuri11ss@gmail.com>
2025-11-17 23:35:05 +0530

    init

---
+++ src/models/user.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+export default User;
